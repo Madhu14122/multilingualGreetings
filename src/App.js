e0b1d9f3afa34ddbff1a928a19aef65b95a7c3bb
Madhu14122 <coolcoder14122@gmail.com>
2023-08-20 13:11:27 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Tabs from './components/Tabs'
+import LanguageItem from './components/LanguageItem'
+
+import './App.css'
+
+const languageGreetingsList = [
+  {
+    id: 'bfdf40eb-eec9-4a66-a493-752fe689f0d0',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png',
+    buttonText: 'English',
+    imageAltText: 'english',
+  },
+  {
+    id: '0ceda891-2a0c-49e2-8c62-68e78180bac6',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/tamil-greetings-img.png',
+    buttonText: 'Tamil',
+    imageAltText: 'tamil',
+  },
+  {
+    id: '89537778-7a46-4c58-988c-0adc931d087c',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/telugu-greetings-img.png',
+    buttonText: 'Telugu',
+    imageAltText: 'telugu',
+  },
+]
+
+class App extends Component {
+  state = {
+    activeTabId: languageGreetingsList[0].id,
+  }
+
+  clickTabItem = tabValue => {
+    this.setState({activeTabId: tabValue})
+  }
+
+  getFilteredProjects = () => {
+    const {activeTabId} = this.state
+    const filteredProjects = languageGreetingsList.filter(
+      eachDetails => eachDetails.id === activeTabId,
+    )
+    return filteredProjects
+  }
+
+  render() {
+    const {activeTabId} = this.state
+    const filteredProjects = this.getFilteredProjects()
+
+    return (
+      <div className="main-container">
+        <h1 className="heading">Multilingual Greetings</h1>
+        <ul className="tabs-container">
+          {languageGreetingsList.map(tabDetails => (
+            <Tabs
+              key={tabDetails.id}
+              tabDetails={tabDetails}
+              clickTabItem={this.clickTabItem}
+              isActive={activeTabId === tabDetails.id}
+            />
+          ))}
+        </ul>
+
+        {filteredProjects.map(eachItem => (
+          <LanguageItem key={eachItem.id} languageDetails={eachItem} />
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
